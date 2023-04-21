32e066752daf968554bb2d7bc0d635e45be442ca
Peter M <pmoyano@solvd.com>
2023-04-21 15:11:13 -0300

    Add completion endpoint

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import openai
 from flask import Flask, render_template, request
 from dotenv import dotenv_values
+import json
 
 # Look for variables in .env and return them in config object
 config = dotenv_values('.env')
 openai.api_key = config["OPENAI_API_KEY"]
+
 #initialize flask
 app = Flask(__name__, template_folder='templates')
+
+#completion call with query
+def get_colors(query):
+    res = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"You are a bot that generates a palette of colors(return between 3 and 6) in response to a text user input. Tex: {query}Respond with an array of strings that hold a hex color related with the input text",
+        max_tokens=500
+    )
+    return res.choices[0].text
 
 #routes
 @app.route("/")
@@ -15,14 +26,19 @@ def index():
         model="text-davinci-003",
         prompt="Give me a funny word."
     )
-    return res["choices"][0]["text"]
+    colors = json.loads(res["choices"][0]["text"])
+    return colors
     # return render_template("index.html")
 
-# @app.route("/palette", methods=["POST"])
-# def prompt_to_palette( ):
-    #openai completion call
+@app.route("/palette", methods=["POST"])
+def prompt_to_palette( ):
+    #extract query string 
+    app.logger.info(request.form.get("query"))
+    query = request.form.get("query")
+    #completion request with query
+    colors = get_colors (query)
 
-    return render_template("index.html")
+    return {"colors": colors}
 
 # debug mode config
 if __name__ == "__main__":
